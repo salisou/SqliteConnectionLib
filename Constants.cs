0a70d2645c2149b1c3188b55fff289bbdf2d8b1a
salisou moussa <salisoumoussa1@gmail.com>
2024-10-18 08:20:38 +0200

    Add constant connectiion

---
+++ Constants.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Storage;
+using SQLite;
+
+namespace SqliteConnectionLib
+{
+	public static class Constants
+	{
+		private const string DbFileName = "Sqlie.db3";
+
+		public const SQLiteOpenFlags flags =
+			SQLiteOpenFlags.ReadWrite |
+			SQLiteOpenFlags.Create |
+			SQLiteOpenFlags.SharedCache;
+
+
+		public static string DatabasePath
+		{
+			get => Path.Combine(FileSystem.AppDataDirectory, DbFileName);
+		}
+	}
+}
