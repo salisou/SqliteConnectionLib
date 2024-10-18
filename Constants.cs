e6b815176da25576cce18fbc38425a15020747f6
salisou moussa <salisoumoussa1@gmail.com>
2024-10-18 15:26:05 +0200

    Documentazione di Constants

--- Constants.cs
+++ Constants.cs
@@ -5,16 +5,20 @@ namespace SqliteConnectionLib
 {
 	public static class Constants
 	{
+		// Nome del file di database SQLite
 		private const string DbFileName = "Sqlie.db3";
 
+		// Flag per gestire le impostazioni della connessione SQLite
 		public const SQLiteOpenFlags flags =
-			SQLiteOpenFlags.ReadWrite |
-			SQLiteOpenFlags.Create |
-			SQLiteOpenFlags.SharedCache;
+			SQLiteOpenFlags.ReadWrite |   // Permette la lettura e scrittura del database
+			SQLiteOpenFlags.Create |      // Crea il database se non esiste
+			SQLiteOpenFlags.SharedCache;  // Permette la condivisione della cache tra connessioni
 
-
+		// Percorso del database
 		public static string DatabasePath
 		{
+			// Restituisce il percorso completo dove il file del database sarà memorizzato
+			// Utilizza 'AppDataDirectory' di MAUI per memorizzare il file in una directory accessibile all'app
 			get => Path.Combine(FileSystem.AppDataDirectory, DbFileName);
 		}
 	}
